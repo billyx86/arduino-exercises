83ce4f861fca8096f5e3949380926aa3abd76963
Billy King <121424512+billyx86@users.noreply.github.com>
2023-04-08 21:42:25 +0100

    Create dimmableLED.ino

---
+++ dimmableLED/dimmableLED/dimmableLED.ino
@@ -0,0 +1,21 @@
+int readPIN { A3 };
+int delayTime { 250 };
+int brightness {};
+int redPin { 9 };
+
+void setup() {
+  pinMode(readPIN, INPUT);
+  pinMode(redPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  brightness = (analogRead(readPIN) / 1023.) * 255;
+  Serial.println(brightness);
+
+  if (brightness < 2) {
+    brightness = 0;
+  }
+  
+  analogWrite(redPin, brightness);
+}
