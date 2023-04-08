6cbe241505c387ca975c182f6e2d6e8806a78caa
Billy King <121424512+billyx86@users.noreply.github.com>
2023-04-08 21:26:14 +0100

    Update potentiometerRead.ino

--- potentiometerRead/potentiometerRead/potentiometerRead.ino
+++ potentiometerRead/potentiometerRead/potentiometerRead.ino
@@ -1,14 +1,25 @@
 int readPIN { A3 };
-int delayTime { 500 };
+int delayTime { 250 };
 float V2 {};
+int redPin { 9 };
 
 void setup() {
   pinMode(readPIN, INPUT);
+  pinMode(redPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   V2 = (5./1023.) * analogRead(readPIN);
-  Serial.println(V2);
+
+  if (V2 > 4) {
+    digitalWrite(redPin, HIGH);
+    Serial.print(V2);
+    Serial.println(", redPin set to HIGH: above 4v!");
+  } else {
+    digitalWrite(redPin, LOW);
+    Serial.println(V2);
+  }
+
   delay(delayTime);
 }
